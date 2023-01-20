e999f9a85dfe9119c33dc069002b359cc4979e8a
adamw <adam@warski.org>
2023-01-20 16:01:34 +0100

    WIP

---
+++ core/src/main/scala/warp/Warp.scala
@@ -0,0 +1,129 @@
+package warp
+
+import jdk.incubator.concurrent.{ScopedValue, StructuredTaskScope}
+
+import java.util.concurrent.{Callable, CompletableFuture}
+import scala.concurrent.{ExecutionException, TimeoutException}
+import scala.concurrent.duration.{Duration, FiniteDuration}
+
+case class Warp[-T](_scope: StructuredTaskScope[_]) {
+  // TODO needed? make _scope private?
+  def scope[U <: T]: StructuredTaskScope[U] = _scope.asInstanceOf[StructuredTaskScope[U]]
+  def cancel(): Unit = scope.shutdown()
+}
+
+object Warp:
+  private class DoNothingScope[T] extends StructuredTaskScope[T](null, Thread.ofVirtual().factory()) {}
+
+  /** Any child fibers are interrupted after `f` completes. */
+  def scoped[T](f: Warp[Any] ?=> T): T = scopedCustom(new DoNothingScope[Any]()) { scope =>
+    try f
+    finally
+      scope.shutdown()
+      scope.join()
+  }
+
+  def scopedCustom[T, S <: StructuredTaskScope[T], U](scope: S)(f: S => Warp[T] ?=> U): U =
+    try f(scope)(using Warp(scope))
+    finally scope.close()
+
+//  trait Sink[T]:
+//    def onComplete(r: Either[Throwable, T]): Unit
+//
+//  def scopedWithSink[T](sink: Sink[T])(t: => T)(using Warp[T]): Fiber[T] = ???
+
+  /** Starts a fiber, which is guaranteed to complete before the enclosing [[scoped]] block exits. */
+  def fork[T](f: => T)(using Warp[T]): Fiber[T] =
+    val result = new CompletableFuture[T]()
+    val forkFuture = summon[Warp[T]].scope.fork { () =>
+      try result.complete(f)
+      catch case e: Throwable => result.completeExceptionally(e)
+
+      null.asInstanceOf // TODO
+    }
+    new Fiber[T]:
+      override def join(): T = result.get()
+      override def cancel(): Either[Throwable, T] =
+        forkFuture.cancel(true)
+        try Right(result.get()) catch
+          case e: ExecutionException => Left(e.getCause)
+          case e: Throwable => Left(e)
+
+  def forkAll[T](fs: Seq[() => T])(using Warp[T]): Fiber[Seq[T]] =
+    val fibers = fs.map(f => fork(f()))
+    new Fiber[Seq[T]]:
+      override def join(): Seq[T] = fibers.map(_.join())
+      override def cancel(): Either[Throwable, Seq[T]] =
+        val results = fibers.map(_.cancel())
+        if results.exists(_.isLeft)
+        then Left(results.collectFirst { case Left(e) => e }.get)
+        else Right(results.collect { case Right(t) => t })
+
+  def timeout[T](duration: FiniteDuration)(t: => T): T =
+    race(t)({ Thread.sleep(duration.toMillis); throw new TimeoutException() })
+
+  def race[T](fs: Seq[() => T]): T =
+    scopedCustom(new StructuredTaskScope.ShutdownOnSuccess[T]()) { scope =>
+      fs.foreach(f => scope.fork(() => f()))
+      scope.join()
+      scope.result()
+    }
+
+  def race[T](f1: => T)(f2: => T): T = race(List(() => f1, () => f2))
+
+  def uninterruptible[T](f: => T): T =
+    scoped {
+      val fiber = fork(f)
+
+      def joinDespiteInterrupted: T =
+        try fiber.join()
+        catch case e: InterruptedException =>
+          joinDespiteInterrupted
+          throw e
+
+      joinDespiteInterrupted
+    }
+
+  def retry[T](times: Int, sleep: FiniteDuration)(f: => T): T =
+    try f
+    catch
+      case e: Throwable =>
+        if times > 0
+        then
+          Thread.sleep(sleep.toMillis)
+          retry(times - 1, sleep)(f)
+        else throw e
+
+  //
+
+  object syntax:
+    extension [T](f: => T)(using Warp[T])
+      def fork: Fiber[T] = Warp.fork(f)
+      def timeout(duration: FiniteDuration): T = Warp.timeout(duration)(f)
+      def scopedWhere[U](fl: FiberLocal[U], u: U): T = fl.scopedWhere(u)(f)
+      def uninterruptible: T = Warp.uninterruptible(f)
+      def raceWith(f2: => T): T = Warp.race(f)(f2)
+      def retry(times: Int, sleep: FiniteDuration): T = Warp.retry(times, sleep)(f)
+
+  //
+
+  trait Fiber[T]:
+    /** Blocks until the fiber completes with a result. Throws an exception, if the fiber completed with an exception. */
+    def join(): T
+
+    /** Interrupts the fiber, and blocks until it completes with a result. */
+    def cancel(): Either[Throwable, T]
+
+  private def scopedValueWhere[T, U](sv: ScopedValue[T], t: T)(f: => U): U =
+    ScopedValue.where(sv, t, (() => f): Callable[U])
+
+  class FiberLocal[T](scopedValue: ScopedValue[T], default: T):
+    def get(): T = scopedValue.orElse(default)
+
+    def scopedWhere[U](newValue: T)(f: Warp[Any] ?=> U): U =
+      // the scoped values need to be set inside the thread that's used to create the new scope, but
+      // before starting the scope itself, as scoped value bindings can't change after the scope is started
+      scopedValueWhere(scopedValue, newValue)(scoped(f))
+
+  object FiberLocal:
+    def apply[T](initialValue: T): FiberLocal[T] = new FiberLocal(ScopedValue.newInstance[T](), initialValue)
---
+++ core/src/test/scala/warp/WarpTest.scala
@@ -0,0 +1,207 @@
+package warp
+
+import jdk.incubator.concurrent.ScopedValue
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.slf4j.LoggerFactory
+import warp.Warp.{fork, retry, scoped, uninterruptible}
+
+import java.time.Clock
+import java.util.concurrent.atomic.AtomicInteger
+import concurrent.duration.DurationInt
+
+class WarpTest extends AnyFlatSpec with Matchers {
+  class Trail(var trail: Vector[String] = Vector.empty) {
+    def add(s: String): Unit = {
+      info(s"[${Clock.systemUTC().instant()}] [${Thread.currentThread().threadId()}] $s")
+      trail = trail :+ s
+    }
+  }
+
+  it should "run two forks concurrently" in {
+    val trail = Trail()
+    scoped {
+      val f1 = fork {
+        Thread.sleep(500)
+        trail.add("f1 complete")
+        5
+      }
+      val f2 = fork {
+        Thread.sleep(1000)
+        trail.add("f2 complete")
+        6
+      }
+      trail.add("main mid")
+      trail.add(s"result = ${f1.join() + f2.join()}")
+    }
+
+    trail.trail shouldBe Vector("main mid", "f1 complete", "f2 complete", "result = 11")
+  }
+
+  it should "allow nested forks" in {
+    val trail = Trail()
+    scoped {
+      val f1 = fork {
+        val f2 = fork {
+          Thread.sleep(1000)
+          trail.add("f2 complete")
+          6
+        }
+
+        Thread.sleep(500)
+        trail.add("f1 complete")
+        5 + f2.join()
+      }
+
+      trail.add("main mid")
+      trail.add(s"result = ${f1.join()}")
+    }
+
+    trail.trail shouldBe Vector("main mid", "f1 complete", "f2 complete", "result = 11")
+  }
+
+  it should "allow extension method syntax" in {
+    import Warp.syntax.*
+    val trail = Trail()
+    scoped {
+      val f1 = {
+        val f2 = {
+          Thread.sleep(1000)
+          trail.add("f2 complete")
+          6
+        }.fork
+
+        Thread.sleep(500)
+        trail.add("f1 complete")
+        5 + f2.join()
+      }.fork
+
+      trail.add("main mid")
+      trail.add(s"result = ${f1.join()}")
+    }
+
+    trail.trail shouldBe Vector("main mid", "f1 complete", "f2 complete", "result = 11")
+  }
+
+  it should "interrupt child fibers when parents complete" in {
+    val trail = Trail()
+    scoped {
+      val f1 = fork {
+        fork {
+          try
+            Thread.sleep(1000)
+            trail.add("f2 complete")
+            6
+          catch
+            case e: InterruptedException =>
+              trail.add("f2 interrupted")
+              throw e
+        }
+
+        Thread.sleep(500)
+        trail.add("f1 complete")
+        5
+      }
+
+      trail.add("main mid")
+      trail.add(s"result = ${f1.join()}")
+    }
+
+    trail.trail shouldBe Vector("main mid", "f1 complete", "result = 5", "f2 interrupted")
+  }
+
+  it should "properly propagate fiber local values" in {
+    val trail = Trail()
+    val v = Warp.FiberLocal("a")
+    scoped {
+      val f1 = fork {
+        v.scopedWhere("x") {
+          Thread.sleep(100L)
+          trail.add(s"In f1 = ${v.get()}")
+        }
+        v.get()
+      }
+
+      val f3 = fork {
+        v.scopedWhere("z") {
+          Thread.sleep(100L)
+          fork {
+            Thread.sleep(100L)
+            trail.add(s"In f3 = ${v.get()}")
+          }.join()
+        }
+        v.get()
+      }
+
+      trail.add("main mid")
+      trail.add(s"result = ${f1.join()}")
+      trail.add(s"result = ${f3.join()}")
+    }
+
+    trail.trail shouldBe Vector("main mid", "In f1 = x", "result = a", "In f3 = z", "result = a")
+  }
+
+  it should "when interrupted, wait until uninterruptible blocks complete" in {
+    val trail = Trail()
+    scoped {
+      val f = fork {
+        trail.add(s"Fork start")
+
+        uninterruptible {
+          trail.add(s"Sleep start")
+          Thread.sleep(2000)
+          trail.add("Sleep done")
+        }
+
+        trail.add("Fork done")
+      }
+
+      Thread.sleep(100)
+      trail.add("Cancelling ...")
+      trail.add(s"Cancel result = ${f.cancel()}")
+    }
+
+    trail.trail shouldBe Vector(
+      "Fork start",
+      "Sleep start",
+      "Cancelling ...",
+      "Sleep done",
+      "Cancel result = Left(java.lang.InterruptedException)"
+    )
+  }
+
+  it should "retry the specified number of times" in {
+    val trail = Trail()
+    val counter = new AtomicInteger(2)
+    scoped {
+      val f = fork {
+        retry(3, 100.milliseconds) {
+          trail.add(s"trying")
+          if counter.getAndDecrement() == 0 then "ok" else throw new RuntimeException("boom")
+        }
+      }
+
+      Thread.sleep(100)
+      trail.add(s"result = ${f.join()}")
+    }
+
+    trail.trail shouldBe Vector("trying", "trying", "trying", "result = ok")
+  }
+}
+
+@main def test1 =
+  val log = LoggerFactory.getLogger("test1")
+  val r = Warp.scoped {
+    val f1 = fork {
+      Thread.sleep(1000L)
+      log.info("f1 done")
+      5
+    }
+    val f2 = fork {
+      Thread.sleep(2000L)
+      log.info("f2 done")
+      6
+    }
+    f1.join() + f2.join()
+  }
+  log.info("result: " + r)
