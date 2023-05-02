53df125d0498a219711baaf0ec523a1656d09e2f
adamw <adam@warski.org>
2023-05-02 10:21:27 +0200

    Timeout tests don't need to be scoped

--- core/src/test/scala/ox/RaceTest.scala
+++ core/src/test/scala/ox/RaceTest.scala
@@ -14,34 +14,30 @@ import scala.concurrent.duration.DurationInt
 class RaceTest extends AnyFlatSpec with Matchers {
   "timeout" should "short-circuit a long computation" in {
     val trail = Trail()
-    scoped {
-      try
-        timeout(1.second) {
-          Thread.sleep(2000)
-          trail.add("no timeout")
-        }
-      catch case _: TimeoutException => trail.add("timeout")
+    try
+      timeout(1.second) {
+        Thread.sleep(2000)
+        trail.add("no timeout")
+      }
+    catch case _: TimeoutException => trail.add("timeout")
 
-      trail.add("done")
-      Thread.sleep(2000)
-    }
+    trail.add("done")
+    Thread.sleep(2000)
 
     trail.trail shouldBe Vector("timeout", "done")
   }
 
   it should "not interrupt a short computation" in {
     val trail = Trail()
-    scoped {
-      try
-        timeout(1.second) {
-          Thread.sleep(500)
-          trail.add("no timeout")
-        }
-      catch case _: TimeoutException => trail.add("timeout")
+    try
+      timeout(1.second) {
+        Thread.sleep(500)
+        trail.add("no timeout")
+      }
+    catch case _: TimeoutException => trail.add("timeout")
 
-      trail.add("done")
-      Thread.sleep(2000)
-    }
+    trail.add("done")
+    Thread.sleep(2000)
 
     trail.trail shouldBe Vector("no timeout", "done")
   }
