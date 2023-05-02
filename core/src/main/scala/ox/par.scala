fcc9be28bd32d46d35fbff358e91e6572b5f7c08
adamw <adam@warski.org>
2023-05-02 10:20:54 +0200

    parLimit

--- core/src/main/scala/ox/par.scala
+++ core/src/main/scala/ox/par.scala
@@ -1,5 +1,7 @@
 package ox
 
+import java.util.concurrent.Semaphore
+
 /** Runs the given computations in parallel. If any fails, interrupts the others, and re-throws the exception. */
 def par[T1, T2](t1: => T1)(t2: => T2): (T1, T2) =
   scoped {
@@ -21,5 +23,23 @@ def par[T1, T2, T3](t1: => T1)(t2: => T2)(t3: => T3): (T1, T2, T3) =
 def par[T](ts: Seq[() => T]): Seq[T] =
   scoped {
     val fs = ts.map(t => fork(t()))
+    fs.map(_.join())
+  }
+
+/** Runs the given computations in parallel, with at most `concurrency` running concurrently at the same time. If any computation fails,
+  * interrupts the others, and re-throws the exception.
+  */
+def parLimit[T](concurrency: Int)(ts: Seq[() => T]): Seq[T] =
+  scoped {
+    val s = new Semaphore(concurrency)
+    val fs = ts.map(t =>
+      fork {
+        s.acquire()
+        val r = t()
+        // no try-finally as there's no point in releasing in case of an exception, as any newly started forks will be interrupted
+        s.release()
+        r
+      }
+    )
     fs.map(_.join())
   }
--- core/src/test/scala/ox/ParTest.scala
+++ core/src/test/scala/ox/ParTest.scala
@@ -14,18 +14,40 @@ import scala.concurrent.duration.DurationInt
 class ParTest extends AnyFlatSpec with Matchers {
   "par" should "run computations in parallel" in {
     val trail = Trail()
-    scoped {
-      par {
-        Thread.sleep(200)
-        trail.add("a")
-      } {
-        Thread.sleep(100)
-        trail.add("b")
-      }
-
-      trail.add("done")
+    par {
+      Thread.sleep(200)
+      trail.add("a")
+    } {
+      Thread.sleep(100)
+      trail.add("b")
     }
 
+    trail.add("done")
+
     trail.trail shouldBe Vector("b", "a", "done")
+  }
+
+  "parLimit" should "run up to the given number of computations in parallel" in {
+    val trail = Trail()
+    scoped {
+      fork {
+        forever {
+          Thread.sleep(200)
+          trail.add("y")
+        }
+      }
+
+      Thread.sleep(100)
+      parLimit(2)(
+        (1 to 5).map(_ =>
+          () => {
+            Thread.sleep(200)
+            trail.add("x")
+          }
+        )
+      )
+    }
+
+    trail.trail shouldBe Vector("y", "x", "x", "y", "x", "x", "y", "x")
   }
 }
