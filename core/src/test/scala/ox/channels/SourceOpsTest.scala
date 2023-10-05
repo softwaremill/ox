07072e5ed42fc94b2c08878b524b2c8267fbe12b
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2023-10-05 10:25:43 +0200

    Move Source.interleave tests to separate class

--- core/src/test/scala/ox/channels/SourceOpsTest.scala
+++ core/src/test/scala/ox/channels/SourceOpsTest.scala
@@ -209,51 +209,6 @@ class SourceOpsTest extends AnyFlatSpec with Matchers with Eventually {
     }
   }
 
-  it should "interleave with an empty source" in scoped {
-    val c1 = Source.fromValues(1, 2, 3)
-    val c2 = Source.fromValues()
-
-    val s1 = c1.interleave(c2)
-
-    s1.toList shouldBe List(1, 2, 3)
-  }
-
-  it should "interleave two sources with default segment size" in scoped {
-    val c1 = Source.fromValues(1, 3, 5)
-    val c2 = Source.fromValues(2, 4, 6)
-
-    val s = c1.interleave(c2)
-
-    s.toList shouldBe List(1, 2, 3, 4, 5, 6)
-  }
-
-  it should "interleave two sources with default segment size and different lengths" in scoped {
-    val c1 = Source.fromValues(1, 3, 5)
-    val c2 = Source.fromValues(2, 4, 6, 8, 10, 12)
-
-    val s = c1.interleave(c2)
-
-    s.toList shouldBe List(1, 2, 3, 4, 5, 6, 8, 10, 12)
-  }
-
-  it should "interleave two sources with custom segment size" in scoped {
-    val c1 = Source.fromValues(1, 2, 3, 4)
-    val c2 = Source.fromValues(10, 20, 30, 40)
-
-    val s = c1.interleave(c2, segmentSize = 2)
-
-    s.toList shouldBe List(1, 2, 10, 20, 3, 4, 30, 40)
-  }
-
-  it should "interleave two sources with custom segment size and different lengths" in scoped {
-    val c1 = Source.fromValues(1, 2, 3, 4, 5, 6, 7)
-    val c2 = Source.fromValues(10, 20, 30, 40)
-
-    val s = c1.interleave(c2, segmentSize = 2)
-
-    s.toList shouldBe List(1, 2, 10, 20, 3, 4, 30, 40, 5, 6, 7)
-  }
-
   it should "merge two sources" in {
     scoped {
       val c1 = Source.fromValues(1, 2, 3)
