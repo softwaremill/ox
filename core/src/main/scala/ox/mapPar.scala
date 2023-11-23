a4bb211ce36e0e2e9fc4666271520d611f502feb
Paweł Stawicki <pawelstawicki@gmail.com>
2023-11-23 12:32:33 +0100

    Added scaladoc to mapPar

--- core/src/main/scala/ox/mapPar.scala
+++ core/src/main/scala/ox/mapPar.scala
@@ -3,6 +3,12 @@ package ox
 import java.util.concurrent.Semaphore
 import scala.collection.IterableFactory
 
+/** Runs parallel transformations on `iterable`. Using not more than `parallelism` forks concurrently.
+ *
+ * @param parallelism maximum number of concurrent forks
+ * @param iterable collection to transform
+ * @param transform transformation to apply to each element of `iterable`
+ */
 def mapPar[I, O, C[E] <: Iterable[E]](parallelism: Int)(iterable: => C[I])(transform: I => O): C[O] =
   val s = Semaphore(parallelism)
 
