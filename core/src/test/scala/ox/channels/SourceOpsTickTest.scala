b699048fc6666c36850381d26343aefb2a9d72f7
adamw <adam@warski.org>
2024-02-26 12:43:51 +0100

    Fix the tick source to emit values at regular intervals, accommodating for slow consumers

---
+++ core/src/test/scala/ox/channels/SourceOpsTickTest.scala
@@ -0,0 +1,48 @@
+package ox.channels
+
+import org.scalatest.concurrent.Eventually
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import ox.*
+
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.atomic.AtomicInteger
+import scala.concurrent.duration.DurationInt
+import scala.jdk.CollectionConverters.*
+
+class SourceOpsTickTest extends AnyFlatSpec with Matchers with Eventually {
+  it should "tick regularly" in {
+    supervised {
+      val c = Source.tick(100.millis)
+      val start = System.currentTimeMillis()
+      c.receive() shouldBe ()
+      (System.currentTimeMillis() - start) shouldBe >=(0L)
+      (System.currentTimeMillis() - start) shouldBe <=(50L)
+
+      c.receive() shouldBe ()
+      (System.currentTimeMillis() - start) shouldBe >=(100L)
+      (System.currentTimeMillis() - start) shouldBe <=(150L)
+
+      c.receive() shouldBe ()
+      (System.currentTimeMillis() - start) shouldBe >=(200L)
+      (System.currentTimeMillis() - start) shouldBe <=(250L)
+    }
+  }
+
+  it should "tick immediately in case of a slow consumer, and then resume normal " in {
+    supervised {
+      val c = Source.tick(100.millis)
+      val start = System.currentTimeMillis()
+
+      // simulating a slow consumer
+      Thread.sleep(200)
+      c.receive() shouldBe () // a tick should be waiting
+      (System.currentTimeMillis() - start) shouldBe >=(200L)
+      (System.currentTimeMillis() - start) shouldBe <=(250L)
+
+      c.receive() shouldBe () // and immediately another, as the interval between send-s has passed
+      (System.currentTimeMillis() - start) shouldBe >=(200L)
+      (System.currentTimeMillis() - start) shouldBe <=(250L)
+    }
+  }
+}
