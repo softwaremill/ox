0ccac23e783f61365a6d2fa3370f5ea92dc09561
adamw <adam@warski.org>
2024-02-27 15:49:31 +0100

    Add additional select variants

--- core/src/main/scala/ox/channels/select.scala
+++ core/src/main/scala/ox/channels/select.scala
@@ -16,6 +16,27 @@ def selectSafe(
 ): clause1.Result | clause2.Result | clause3.Result | ChannelClosed =
   selectSafe(List(clause1, clause2, clause3)).asInstanceOf[clause1.Result | clause2.Result | clause3.Result | ChannelClosed]
 
+/** @see [[selectSafe(List[SelectClause])]]. */
+def selectSafe(
+    clause1: SelectClause[_],
+    clause2: SelectClause[_],
+    clause3: SelectClause[_],
+    clause4: SelectClause[_]
+): clause1.Result | clause2.Result | clause3.Result | clause4.Result | ChannelClosed =
+  selectSafe(List(clause1, clause2, clause3, clause4))
+    .asInstanceOf[clause1.Result | clause2.Result | clause3.Result | clause4.Result | ChannelClosed]
+
+/** @see [[selectSafe(List[SelectClause])]]. */
+def selectSafe(
+    clause1: SelectClause[_],
+    clause2: SelectClause[_],
+    clause3: SelectClause[_],
+    clause4: SelectClause[_],
+    clause5: SelectClause[_]
+): clause1.Result | clause2.Result | clause3.Result | clause4.Result | clause5.Result | ChannelClosed =
+  selectSafe(List(clause1, clause2, clause3, clause4, clause5))
+    .asInstanceOf[clause1.Result | clause2.Result | clause3.Result | clause4.Result | clause5.Result | ChannelClosed]
+
 /** Select exactly one clause to complete. Each clause should be created for a different channel. Clauses can be created using
   * [[Source.receiveClause]], [[Sink.sendClause]] and [[Default]].
   *
@@ -47,6 +68,26 @@ def select(
     clause3: SelectClause[_]
 ): clause1.Result | clause2.Result | clause3.Result =
   select(List(clause1, clause2, clause3)).asInstanceOf[clause1.Result | clause2.Result | clause3.Result]
+
+/** @see [[select(List[SelectClause])]]. */
+def select(
+    clause1: SelectClause[_],
+    clause2: SelectClause[_],
+    clause3: SelectClause[_],
+    clause4: SelectClause[_]
+): clause1.Result | clause2.Result | clause3.Result | clause4.Result =
+  select(List(clause1, clause2, clause3, clause4)).asInstanceOf[clause1.Result | clause2.Result | clause3.Result | clause4.Result]
+
+/** @see [[select(List[SelectClause])]]. */
+def select(
+    clause1: SelectClause[_],
+    clause2: SelectClause[_],
+    clause3: SelectClause[_],
+    clause4: SelectClause[_],
+    clause5: SelectClause[_]
+): clause1.Result | clause2.Result | clause3.Result | clause4.Result | clause5.Result =
+  select(List(clause1, clause2, clause3, clause4, clause5))
+    .asInstanceOf[clause1.Result | clause2.Result | clause3.Result | clause4.Result | clause5.Result]
 
 /** Select exactly one clause to complete. Each clause should be created for a different channel. Clauses can be created using
   * [[Source.receiveClause]], [[Sink.sendClause]] and [[Default]].
@@ -81,6 +122,32 @@ def selectSafe[T1, T2, T3](source1: Source[T1], source2: Source[T2], source3: So
     case source1.Received(v) => v
     case source2.Received(v) => v
     case source3.Received(v) => v
+  }
+
+/** @see [[selectSafe(List[Source])]]. */
+def selectSafe[T1, T2, T3, T4](source1: Source[T1], source2: Source[T2], source3: Source[T3], source4: Source[T4]): T1 | T2 | T3 | T4 |
+  ChannelClosed =
+  selectSafe(source1.receiveClause, source2.receiveClause, source3.receiveClause, source4.receiveClause).map {
+    case source1.Received(v) => v
+    case source2.Received(v) => v
+    case source3.Received(v) => v
+    case source4.Received(v) => v
+  }
+
+/** @see [[selectSafe(List[Source])]]. */
+def selectSafe[T1, T2, T3, T4, T5](
+    source1: Source[T1],
+    source2: Source[T2],
+    source3: Source[T3],
+    source4: Source[T4],
+    source5: Source[T5]
+): T1 | T2 | T3 | T4 | T5 | ChannelClosed =
+  selectSafe(source1.receiveClause, source2.receiveClause, source3.receiveClause, source4.receiveClause, source5.receiveClause).map {
+    case source1.Received(v) => v
+    case source2.Received(v) => v
+    case source3.Received(v) => v
+    case source4.Received(v) => v
+    case source5.Received(v) => v
   }
 
 /** Select exactly one source, from which to receive a value. Sources should not repeat. Clauses can be created using
@@ -119,6 +186,31 @@ def select[T1, T2, T3](source1: Source[T1], source2: Source[T2], source3: Source
     case source1.Received(v) => v
     case source2.Received(v) => v
     case source3.Received(v) => v
+  }
+
+/** @see [[select(List[Source])]]. */
+def select[T1, T2, T3, T4](source1: Source[T1], source2: Source[T2], source3: Source[T3], source4: Source[T4]): T1 | T2 | T3 | T4 =
+  select(source1.receiveClause, source2.receiveClause, source3.receiveClause, source4.receiveClause) match {
+    case source1.Received(v) => v
+    case source2.Received(v) => v
+    case source3.Received(v) => v
+    case source4.Received(v) => v
+  }
+
+/** @see [[select(List[Source])]]. */
+def select[T1, T2, T3, T4, T5](
+    source1: Source[T1],
+    source2: Source[T2],
+    source3: Source[T3],
+    source4: Source[T4],
+    source5: Source[T5]
+): T1 | T2 | T3 | T4 | T5 =
+  select(source1.receiveClause, source2.receiveClause, source3.receiveClause, source4.receiveClause, source5.receiveClause) match {
+    case source1.Received(v) => v
+    case source2.Received(v) => v
+    case source3.Received(v) => v
+    case source4.Received(v) => v
+    case source5.Received(v) => v
   }
 
 /** Select exactly one source, from which to receive a value. Sources should not repeat. Clauses can be created using
