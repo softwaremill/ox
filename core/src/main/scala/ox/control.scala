3956b9559dabf82927a29cbbbb02b94db02e23ae
adamw <adam@warski.org>
2024-03-20 13:00:16 +0100

    Use inline parameters instead of by-name methods

--- core/src/main/scala/ox/control.scala
+++ core/src/main/scala/ox/control.scala
@@ -2,21 +2,21 @@ package ox
 
 import java.util.concurrent.locks.LockSupport
 
-inline def forever(f: => Unit): Nothing =
+inline def forever(inline f: Unit): Nothing =
   while true do f
   throw new RuntimeException("can't get here")
 
 /** Repeat evaluating `f` while it evaluates to `true`. */
-inline def repeatWhile(f: => Boolean): Unit =
+inline def repeatWhile(inline f: Boolean): Unit =
   var loop = true
   while loop do loop = f
 
 /** Repeat evaluating `f` until it evaluates to `true`. */
-inline def repeatUntil(f: => Boolean): Unit =
+inline def repeatUntil(inline f: Boolean): Unit =
   var loop = true
   while loop do loop = !f
 
-inline def uninterruptible[T](f: => T): T =
+inline def uninterruptible[T](inline f: T): T =
   scoped {
     val t = fork(f)
 
