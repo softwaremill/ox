bed780f05e1ab69d4f75ad02edba94342fc25147
adamw <adam@warski.org>
2024-04-03 16:09:26 +0200

    Swap order of parameters in retry

--- core/src/test/scala/ox/retry/DelayedRetryTest.scala
+++ core/src/test/scala/ox/retry/DelayedRetryTest.scala
@@ -22,7 +22,7 @@ class DelayedRetryTest extends AnyFlatSpec with Matchers with EitherValues with 
       if true then throw new RuntimeException("boom")
 
     // when
-    val (result, elapsedTime) = measure(the[RuntimeException] thrownBy retry(f)(RetryPolicy.delay(maxRetries, sleep)))
+    val (result, elapsedTime) = measure(the[RuntimeException] thrownBy retry(RetryPolicy.delay(maxRetries, sleep))(f))
 
     // then
     result should have message "boom"
@@ -42,7 +42,7 @@ class DelayedRetryTest extends AnyFlatSpec with Matchers with EitherValues with 
       if counter <= retriesUntilSuccess then throw new RuntimeException("boom") else successfulResult
 
     // when
-    val result = retry(f)(RetryPolicy.delayForever(sleep))
+    val result = retry(RetryPolicy.delayForever(sleep))(f)
 
     // then
     result shouldBe successfulResult
@@ -61,7 +61,7 @@ class DelayedRetryTest extends AnyFlatSpec with Matchers with EitherValues with 
       Left(errorMessage)
 
     // when
-    val (result, elapsedTime) = measure(retryEither(f)(RetryPolicy.delay(maxRetries, sleep)))
+    val (result, elapsedTime) = measure(retryEither(RetryPolicy.delay(maxRetries, sleep))(f))
 
     // then
     result.left.value shouldBe errorMessage
