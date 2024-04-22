936cb3bfe8da9a978b475db0d8b931a99d63baae
Adam Warski <adam@warski.org>
2024-04-22 14:36:13 +0200

    Rename .value to .ok(), add .fail() (#127)

--- core/src/test/scala/ox/EitherTest.scala
+++ core/src/test/scala/ox/EitherTest.scala
@@ -1,9 +1,9 @@
 package ox
 
+import org.scalatest.exceptions.TestFailedException
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-
-import ox.either.value
+import ox.either.{ok, fail}
 
 class EitherTest extends AnyFlatSpec with Matchers:
   val ok1: Either[Int, String] = Right("x")
@@ -17,34 +17,86 @@ class EitherTest extends AnyFlatSpec with Matchers:
   val optionFail: Option[Int] = None
 
   "either" should "work correctly when invoked on eithers" in {
-    val r1 = either((ok1.value, ok2.value))
+    val r1 = either((ok1.ok(), ok2.ok()))
     r1 shouldBe Right(("x", "y"))
 
     //
 
-    val r2 = either((ok1.value, fail1.value))
+    val r2 = either((ok1.ok(), fail1.ok()))
     r2 shouldBe Left(1)
 
     //
 
-    val r3 = either((fail1.value, fail2.value))
+    val r3 = either((fail1.ok(), fail2.ok()))
     r3 shouldBe Left(1)
 
     //
 
-    val r4 = either((ok1.value, ok3.value))
+    val r4 = either((ok1.ok(), ok3.ok()))
     r4 shouldBe Right(("x", 50L))
 
     //
 
-    val r5: Either[Int | String, (String, String)] = either((fail3.value, fail1.value))
+    val r5: Either[Int | String, (String, String)] = either((fail3.ok(), fail1.ok()))
     r5 shouldBe Left("x")
+
+    //
+
+    val r6 = either("x".fail())
+    r6 shouldBe Left("x")
   }
 
   it should "work correctly when invoked on options" in {
-    val r1 = either((optionOk1.value, optionOk2.value))
+    val r1 = either((optionOk1.ok(), optionOk2.ok()))
     r1 shouldBe (Right(10, "x"))
 
-    val r2 = either((optionOk1.value, optionFail.value))
+    val r2 = either((optionOk1.ok(), optionFail.ok()))
     r2 shouldBe Left(())
+
+    val r3: Either[Unit, (Int, Int)] = either((optionOk1.ok(), optionFail.ok()))
+    r3 shouldBe Left(())
+  }
+
+  it should "report a proper compilation error when used outside of either:" in {
+    val e = intercept[TestFailedException](assertCompiles("ok1.ok()"))
+
+    e.getMessage should include("`.ok()` can only be used within an `either` call.")
+  }
+
+  it should "report a proper compilation error when wrong error type is used for ok() (explicit type params)" in {
+    val e = intercept[TestFailedException](assertCompiles("either[String, String](fail1.ok())"))
+
+    e.getMessage should include("The enclosing `either` call uses a different error type.")
+  }
+
+  it should "report a proper compilation error when wrong successful type is used (explicit type params)" in {
+    val e = intercept[TestFailedException](assertCompiles("either[Int, Int](fail1.ok())"))
+
+    e.getMessage should include("Found:    String")
+    e.getMessage should include("Required: Int")
+  }
+
+  it should "report a proper compilation error when wrong type annotation is used for ok() (error)" in {
+    val e = intercept[TestFailedException](assertCompiles("val r: Either[String, String] = either(fail1.ok())"))
+
+    e.getMessage should include("The enclosing `either` call uses a different error type.")
+  }
+
+  it should "report a proper compilation error when wrong type annotation is used (success)" in {
+    val e = intercept[TestFailedException](assertCompiles("val r: Either[Int, Int] = either(fail1.ok())"))
+
+    e.getMessage should include("Found:    String")
+    e.getMessage should include("Required: Int")
+  }
+
+  it should "report a proper compilation error when wrong error type is used for fail() (explicit type params)" in {
+    val e = intercept[TestFailedException](assertCompiles("""either[Int, Int]("x".fail())"""))
+
+    e.getMessage should include("The enclosing `either` call uses a different error type.")
+  }
+
+  it should "report a proper compilation error when wrong type annotation is used for fail() (error)" in {
+    val e = intercept[TestFailedException](assertCompiles("""val r: Either[Int, Int] = either("x".fail())"""))
+
+    e.getMessage should include("The enclosing `either` call uses a different error type.")
   }
