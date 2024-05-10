811745e546a433548d41cfb1774945275daa4f7d
kciesielski <kciesielski@users.noreply.github.com>
2024-05-10 12:14:18 +0200

    Remove the 'text' wrapper

--- core/src/main/scala/ox/channels/SourceTextOps.scala
+++ core/src/main/scala/ox/channels/SourceTextOps.scala
@@ -6,26 +6,25 @@ import scala.annotation.tailrec
 trait SourceTextOps[+T]:
   outer: Source[T] =>
 
-  object text:
-    def lines(using Ox, T <:< Chunk[Byte]): Source[String] =
-      // buffer == null is a special state for handling empty chunks in onComplete, in order to tell them apart from empty lines
-      outer
-        .mapStatefulConcat(() => null: Chunk[Byte])(
-          { case (buffer, nextChunk) =>
-            @tailrec
-            def splitChunksAtNewLine(buf: Chunk[Byte], chunk: Chunk[Byte], acc: Vector[Chunk[Byte]]): (Chunk[Byte], Vector[Chunk[Byte]]) =
-              val newlineIdx = chunk.indexWhere(_ == '\n')
-              if newlineIdx == -1 then (buf ++ chunk, acc)
-              else
-                val (chunk1, chunk2) = chunk.splitAt(newlineIdx)
-                splitChunksAtNewLine(Chunk.empty, chunk2.drop(1), acc :+ (if buffer != null then buffer ++ chunk1 else chunk1))
+  def lines(using Ox, T <:< Chunk[Byte]): Source[String] =
+    // buffer == null is a special state for handling empty chunks in onComplete, in order to tell them apart from empty lines
+    outer
+      .mapStatefulConcat(() => null: Chunk[Byte])(
+        { case (buffer, nextChunk) =>
+          @tailrec
+          def splitChunksAtNewLine(buf: Chunk[Byte], chunk: Chunk[Byte], acc: Vector[Chunk[Byte]]): (Chunk[Byte], Vector[Chunk[Byte]]) =
+            val newlineIdx = chunk.indexWhere(_ == '\n')
+            if newlineIdx == -1 then (buf ++ chunk, acc)
+            else
+              val (chunk1, chunk2) = chunk.splitAt(newlineIdx)
+              splitChunksAtNewLine(Chunk.empty, chunk2.drop(1), acc :+ (if buffer != null then buffer ++ chunk1 else chunk1))
 
-            val (newBuffer, toEmit) =
-              if nextChunk.length == 0 then (null, Vector.empty)
-              else splitChunksAtNewLine(if buffer == null then Empty else buffer, nextChunk, Vector.empty)
+          val (newBuffer, toEmit) =
+            if nextChunk.length == 0 then (null, Vector.empty)
+            else splitChunksAtNewLine(if buffer == null then Empty else buffer, nextChunk, Vector.empty)
 
-            (newBuffer, toEmit)
-          },
-          onComplete = buf => if buf != null then Some(buf) else None
-        )
-        .mapAsView(_.asString)
+          (newBuffer, toEmit)
+        },
+        onComplete = buf => if buf != null then Some(buf) else None
+      )
+      .mapAsView(_.asString)
--- core/src/test/scala/ox/channels/SourceTextOpsTest.scala
+++ core/src/test/scala/ox/channels/SourceTextOpsTest.scala
@@ -10,31 +10,31 @@ class SourceTextOpsTest extends AnyFlatSpec with Matchers {
   it should "split a single chunk of bytes into lines" in supervised {
     val inputText = "line1\nline2\nline3"
     val chunk = Chunk.fromArray(inputText.getBytes)
-    Source.fromValues(chunk).text.lines.toList shouldBe List("line1", "line2", "line3")
+    Source.fromValues(chunk).lines.toList shouldBe List("line1", "line2", "line3")
   }
 
   it should "split a single chunk of bytes into lines (multiple newlines)" in supervised {
     val inputText = "line1\n\nline2\nline3"
     val chunk = Chunk.fromArray(inputText.getBytes)
-    Source.fromValues(chunk).text.lines.toList shouldBe List("line1", "", "line2", "line3")
+    Source.fromValues(chunk).lines.toList shouldBe List("line1", "", "line2", "line3")
   }
 
   it should "split a single chunk of bytes into lines (beginning with newline)" in supervised {
     val inputText = "\nline1\nline2"
     val chunk = Chunk.fromArray(inputText.getBytes)
-    Source.fromValues(chunk).text.lines.toList shouldBe List("", "line1", "line2")
+    Source.fromValues(chunk).lines.toList shouldBe List("", "line1", "line2")
   }
 
   it should "split a single chunk of bytes into lines (ending with newline)" in supervised {
     val inputText = "line1\nline2\n"
     val chunk = Chunk.fromArray(inputText.getBytes)
-    Source.fromValues(chunk).text.lines.toList shouldBe List("line1", "line2", "")
+    Source.fromValues(chunk).lines.toList shouldBe List("line1", "line2", "")
   }
 
   it should "split a single chunk of bytes into lines (empty array)" in supervised {
     val inputText = ""
     val chunk = Chunk.fromArray(inputText.getBytes)
-    Source.fromValues(chunk).text.lines.toList shouldBe List.empty
+    Source.fromValues(chunk).lines.toList shouldBe List.empty
   }
 
   it should "split a multiple chunks of bytes into lines" in supervised {
@@ -42,7 +42,7 @@ class SourceTextOpsTest extends AnyFlatSpec with Matchers {
     val chunk1 = Chunk.fromArray(inputText1.getBytes)
     val inputText2 = "line1-part2\nline2"
     val chunk2 = Chunk.fromArray(inputText2.getBytes)
-    Source.fromValues(chunk1, chunk2).text.lines.toList shouldBe List("line1-part1,line1-part2", "line2")
+    Source.fromValues(chunk1, chunk2).lines.toList shouldBe List("line1-part1,line1-part2", "line2")
   }
 
   it should "split a multiple chunks of bytes into lines (multiple newlines)" in supervised {
@@ -52,12 +52,12 @@ class SourceTextOpsTest extends AnyFlatSpec with Matchers {
     val chunk2 = Chunk.fromArray(inputText2.getBytes)
     val inputText3 = "\n"
     val chunk3 = Chunk.fromArray(inputText3.getBytes)
-    Source.fromValues(chunk1, chunk2, chunk3).text.lines.toList shouldBe List("line1-part1,line1-part2", "", "")
+    Source.fromValues(chunk1, chunk2, chunk3).lines.toList shouldBe List("line1-part1,line1-part2", "", "")
   }
 
   it should "split a multiple chunks of bytes into lines (multiple empty chunks)" in supervised {
     val emptyChunk = Chunk.fromArray(Array.empty[Byte])
     val chunk1 = Chunk.fromArray("\n\n".getBytes)
-    Source.fromValues(emptyChunk, emptyChunk, chunk1, emptyChunk).text.lines.toList shouldBe List("", "")
+    Source.fromValues(emptyChunk, emptyChunk, chunk1, emptyChunk).lines.toList shouldBe List("", "")
   }
 }
