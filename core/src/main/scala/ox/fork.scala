7ecf81f374845a85ce7d87e85cdb94cd7feaf0d3
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2024-07-01 14:04:44 +0200

    improve discernment between forked interrupted exception and local interrupted exception

--- core/src/main/scala/ox/fork.scala
+++ core/src/main/scala/ox/fork.scala
@@ -122,7 +122,7 @@ def forkAll[T](fs: Seq[() => T])(using Ox): Fork[Seq[T]] =
   val forks = fs.map(f => fork(f()))
   new Fork[Seq[T]]:
     override def join(): Seq[T] = forks.map(_.join())
-    override def wasInterrupted: Boolean = forks.forall(_.wasInterrupted)
+    override def wasInterruptedWith(ie: InterruptedException): Boolean = forks.exists(_.wasInterruptedWith(ie))
 
 /** Starts a fork (logical thread of execution), which is guaranteed to complete before the enclosing [[supervised]], [[supervisedError]] or
   * [[unsupervised]] block completes, and which can be cancelled on-demand.
@@ -178,12 +178,13 @@ def forkCancellable[T](f: => T)(using OxUnsupervised): CancellableFork[T] =
       if !started.getAndSet(true)
       then result.completeExceptionally(new InterruptedException("fork was cancelled before it started")).discard
 
-    override def wasInterrupted: Boolean = result.isCompletedExceptionally && result.exceptionNow().isInstanceOf[InterruptedException]
+    override def wasInterruptedWith(ie: InterruptedException): Boolean =
+      result.isCompletedExceptionally && (result.exceptionNow() eq ie)
 
 private def newForkUsingResult[T](result: CompletableFuture[T]): Fork[T] = new Fork[T]:
   override def join(): T = unwrapExecutionException(result.get())
-  override def wasInterrupted: Boolean =
-    result.isCompletedExceptionally && result.exceptionNow().isInstanceOf[InterruptedException]
+  override def wasInterruptedWith(ie: InterruptedException): Boolean =
+    result.isCompletedExceptionally && (result.exceptionNow() eq ie)
 
 private[ox] inline def unwrapExecutionException[T](f: => T): T =
   try f
@@ -215,21 +216,21 @@ trait Fork[T]:
     catch
       // normally IE is fatal, but here it could have meant that the fork was cancelled, hence we catch it
       // we do discern between the fork and the current thread being cancelled and rethrow if it's us who's getting the axe
-      case e: InterruptedException => if wasInterrupted then Left(e) else throw e
+      case e: InterruptedException => if wasInterruptedWith(e) then Left(e) else throw e
       case NonFatal(e)             => Left(e)
 
-  def wasInterrupted: Boolean
+  def wasInterruptedWith(ie: InterruptedException): Boolean
 
 object Fork:
   /** A dummy pretending to represent a fork which successfully completed with the given value. */
   def successful[T](value: T): Fork[T] = new Fork[T]:
     override def join(): T = value
-    override def wasInterrupted: Boolean = false
+    override def wasInterruptedWith(ie: InterruptedException): Boolean = false
 
   /** A dummy pretending to represent a fork which failed with the given exception. */
   def failed[T](e: Throwable): Fork[T] = new Fork[T]:
     override def join(): T = throw e
-    override def wasInterrupted: Boolean = e.isInstanceOf[InterruptedException]
+    override def wasInterruptedWith(ie: InterruptedException): Boolean = e eq ie
 
 /** A fork started using [[forkCancellable]], backed by a (virtual) thread. */
 trait CancellableFork[T] extends Fork[T]:
