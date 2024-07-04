373ee388b9c43a35e2828341a7461764962855b3
Michał Ossowski <michal.ossowski@softwaremill.com>
2024-07-04 15:11:21 +0200

    Schedule doc cleanup and disambiguation

--- core/src/main/scala/ox/scheduling/Schedule.scala
+++ core/src/main/scala/ox/scheduling/Schedule.scala
@@ -4,6 +4,7 @@ import scala.concurrent.duration.*
 import scala.util.Random
 
 sealed trait Schedule:
+  // TODO: consider better name that `attempt`
   def nextDelay(attempt: Int, lastDelay: Option[FiniteDuration], lastStartTimestamp: Option[Long]): FiniteDuration
 
 object Schedule:
@@ -16,56 +17,79 @@ object Schedule:
 
   private[scheduling] sealed trait Infinite extends Schedule
 
+  /** A schedule that represents an initial delay applied before the first invocation of operation being scheduled. Usually used in
+    * combination with other schedules using [[fallbackTo]]
+    *
+    * @param delay
+    *   The initial delay.
+    * @example
+    *   {{{
+    *   Schedule.InitialDelay(1.second).fallbackTo(Schedule.Delay.forever(100.millis))
+    *   }}}
+    */
   case class InitialDelay private[scheduling] (delay: FiniteDuration) extends Finite:
     override def maxRepeats: Int = 0
     override def nextDelay(attempt: Int, lastDelay: Option[FiniteDuration], lastStartTimestamp: Option[Long]): FiniteDuration =
       Duration.Zero
     override def initialDelay: FiniteDuration = delay
 
-  /** A schedule that repeats up to a given number of times, with no delay between subsequent attempts.
+  /** A schedule that repeats up to a given number of times, with no delay between subsequent repeats.
     *
     * @param maxRepeats
-    *   The maximum number of attempts.
+    *   The maximum number of repeats.
     */
   case class Immediate(maxRepeats: Int) extends Finite:
     override def nextDelay(attempt: Int, lastDelay: Option[FiniteDuration], lastStartTimestamp: Option[Long]): FiniteDuration =
       Duration.Zero
 
   object Immediate:
-    /** A schedule that repeats indefinitely, with no delay between subsequent attempts. */
+    /** A schedule that repeats indefinitely, with no delay between subsequent repeats. */
     def forever: Infinite = ImmediateForever
 
   private case object ImmediateForever extends Infinite:
     override def nextDelay(attempt: Int, lastDelay: Option[FiniteDuration], lastStartTimestamp: Option[Long]): FiniteDuration =
       Duration.Zero
 
-  /** A schedule that repeats up to a given number of times, with a fixed delay between subsequent attempts.
+  /** A schedule that repeats up to a given number of times, with a fixed delay between subsequent repeats.
     *
     * @param maxRepeats
-    *   The maximum number of attempts.
+    *   The maximum number of repeats.
     * @param delay
-    *   The delay between subsequent attempts.
+    *   The delay between subsequent repeats.
     */
   case class Delay(maxRepeats: Int, delay: FiniteDuration) extends Finite:
     override def nextDelay(attempt: Int, lastDelay: Option[FiniteDuration], lastStartTimestamp: Option[Long]): FiniteDuration = delay
 
   object Delay:
-    /** A schedule that repeats indefinitely, with a fixed delay between subsequent attempts.
+    /** A schedule that repeats indefinitely, with a fixed delay between subsequent repeats.
       *
       * @param delay
-      *   The delay between subsequent attempts.
+      *   The delay between subsequent repeats.
       */
     def forever(delay: FiniteDuration): Infinite = DelayForever(delay)
 
-  case class DelayForever private[scheduling] (delay: FiniteDuration) extends Infinite:
+  private[scheduling] case class DelayForever(delay: FiniteDuration) extends Infinite:
     override def nextDelay(attempt: Int, lastDelay: Option[FiniteDuration], lastStartTimestamp: Option[Long]): FiniteDuration = delay
 
-  // TODO: doc
+  /** A schedule that repeats up to a given number of times, with a fixed rate between subsequent repeats. If the previous operation took
+    * longer than the interval, the next operation will be scheduled immediately.
+    *
+    * @param maxRepeats
+    *   The maximum number of repeats.
+    * @param interval
+    *   The interval between subsequent operations.
+    */
   case class FixedRate(maxRepeats: Int, interval: FiniteDuration) extends Finite:
     override def nextDelay(attempt: Int, lastDelay: Option[FiniteDuration], lastStartTimestamp: Option[Long]): FiniteDuration =
       FixedRate.nextDelay(interval, lastStartTimestamp)
 
   object FixedRate:
+    /** A schedule that repeats indefinitely, with a fixed rate between subsequent repeats. If the previous operation took longer than the
+      * interval, the next operation will be scheduled immediately.
+      *
+      * @param interval
+      *   The interval between subsequent operations.
+      */
     def forever(interval: FiniteDuration): Infinite = FixedRateForever(interval)
 
     private[scheduling] def nextDelay(interval: FiniteDuration, lastStartTimestamp: Option[Long]) =
@@ -77,23 +101,23 @@ object Schedule:
           else Duration.Zero
         case None => interval
 
-  case class FixedRateForever private[scheduling] (interval: FiniteDuration) extends Infinite:
+  private[scheduling] case class FixedRateForever(interval: FiniteDuration) extends Infinite:
     override def nextDelay(attempt: Int, lastDelay: Option[FiniteDuration], lastStartTimestamp: Option[Long]): FiniteDuration =
       FixedRate.nextDelay(interval, lastStartTimestamp)
 
-  /** A schedule that repeats up to a given number of times, with an exponentially increasing delay between subsequent attempts.
+  /** A schedule that repeats up to a given number of times, with an exponentially increasing delay between subsequent repeats.
     *
     * The delay is exponential with base 2 (i.e. the next delay is twice as long as the previous one), starting at the given initial delay
     * and capped at the given maximum delay.
     *
     * @param maxRepeats
-    *   The maximum number of attempts.
+    *   The maximum number of repeats.
     * @param firstDelay
-    *   The delay before the first attempt.
+    *   The delay before the first repeat.
     * @param maxDelay
-    *   The maximum delay between subsequent attempts.
+    *   The maximum delay between subsequent repeats.
     * @param jitter
-    *   A random factor used for calculating the delay between subsequent attempts. See [[Jitter]] for more details. Defaults to no jitter,
+    *   A random factor used for calculating the delay between subsequent repeats. See [[Jitter]] for more details. Defaults to no jitter,
     *   i.e. an exponential backoff with no adjustments.
     */
   case class Exponential(
@@ -130,18 +154,18 @@ object Schedule:
           val last = lastDelay.getOrElse(firstDelay).toMillis
           Random.between(firstDelay.toMillis, last * 3).millis
 
-    /** A schedule that repeats indefinitely, with an exponentially increasing delay between subsequent attempts.
+    /** A schedule that repeats indefinitely, with an exponentially increasing delay between subsequent repeats.
       *
       * The delay is exponential with base 2 (i.e. the next delay is twice as long as the previous one), starting at the given initial delay
       * and capped at the given maximum delay.
       *
       * @param firstDelay
-      *   The delay before the first attempt.
+      *   The delay before the first repeat.
       * @param maxDelay
-      *   The maximum delay between subsequent attempts.
+      *   The maximum delay between subsequent repeats.
       * @param jitter
-      *   A random factor used for calculating the delay between subsequent attempts. See [[Jitter]] for more details. Defaults to no
-      *   jitter, i.e. an exponential backoff with no adjustments.
+      *   A random factor used for calculating the delay between subsequent repeats. See [[Jitter]] for more details. Defaults to no jitter,
+      *   i.e. an exponential backoff with no adjustments.
       */
     def forever(firstDelay: FiniteDuration, maxDelay: FiniteDuration = 1.minute, jitter: Jitter = Jitter.None): Infinite =
       ExponentialForever(firstDelay, maxDelay, jitter)
@@ -169,7 +193,7 @@ object Schedule:
     override def initialDelay: FiniteDuration = base.initialDelay
 
   object FallingBack:
-    /** A schedule that repeats indefinitely, using [[base]] first [[base.maxAttempts]] number of times, and then always using [[fallback]].
+    /** A schedule that repeats indefinitely, using [[base]] first [[base.maxRepeats]] number of times, and then always using [[fallback]].
       */
     def forever(base: Finite, fallback: Infinite): Infinite = FallingBackForever(base, fallback)
 
