68647b622c97d392ba79fb87aba829f5810249fc
adamw <adam@warski.org>
2024-07-18 13:56:05 +0200

    Retain the execution exception as a suppressed exception, not to loose context when throwing exceptions in forks

--- core/src/test/scala/ox/ExceptionTest.scala
+++ core/src/test/scala/ox/ExceptionTest.scala
@@ -29,6 +29,14 @@ class ExceptionTest extends AnyFlatSpec with Matchers:
     trail.get shouldBe Vector("CustomException")
   }
 
+  it should "retain the original exception for context, as suppressed" in {
+    val trail = Trail()
+    try supervised(throw CustomException())
+    catch case e: Exception => addExceptionWithSuppressedTo(trail, e)
+
+    trail.get shouldBe Vector("CustomException(suppressed=ExecutionException)")
+  }
+
   it should "throw the exception thrown by a failing fork" in {
     val trail = Trail()
     try supervised(forkUser(throw CustomException()))
@@ -55,7 +63,7 @@ class ExceptionTest extends AnyFlatSpec with Matchers:
       }
     catch case e: Exception => addExceptionWithSuppressedTo(trail, e)
 
-    trail.get shouldBe Vector("CustomException(suppressed=InterruptedException,InterruptedException)")
+    trail.get shouldBe Vector("CustomException(suppressed=ExecutionException,InterruptedException,InterruptedException)")
   }
 
   it should "interrupt other forks when there's a failure, add suppressed custom exceptions" in {
@@ -74,7 +82,7 @@ class ExceptionTest extends AnyFlatSpec with Matchers:
       }
     catch case e: Exception => addExceptionWithSuppressedTo(trail, e)
 
-    trail.get shouldBe Vector("CustomException(suppressed=CustomException2)")
+    trail.get shouldBe Vector("CustomException(suppressed=ExecutionException,CustomException2)")
   }
 
   it should "not add the original exception as suppressed" in {
@@ -91,7 +99,9 @@ class ExceptionTest extends AnyFlatSpec with Matchers:
 
       // either join() might throw the original exception (shouldn't be suppressed), or it might be interrupted before
       // throwing (should be suppressed then)
-    trail.get should (be(Vector("CustomException(suppressed=)")) or be(Vector("CustomException(suppressed=InterruptedException)")))
+    trail.get should (be(Vector("CustomException(suppressed=ExecutionException)")) or be(
+      Vector("CustomException(suppressed=ExecutionException,InterruptedException)")
+    ))
   }
 
   it should "add an exception as suppressed, even if it wraps the original exception" in {
@@ -108,7 +118,7 @@ class ExceptionTest extends AnyFlatSpec with Matchers:
       }
     catch case e: Exception => addExceptionWithSuppressedTo(trail, e)
 
-    trail.get shouldBe Vector("CustomException(suppressed=CustomException3)")
+    trail.get shouldBe Vector("CustomException(suppressed=ExecutionException,CustomException3)")
   }
 
   "joinEither" should "catch the exception with which a fork ends" in {
