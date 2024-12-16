6e9ce3d53e7e17f9a3d172ca593a99a099224b8a
corem <cornet.remi@gmail.com>
2024-12-16 17:02:13 -0500

    FlowOps: debounceBy operator

--- core/src/main/scala/ox/flow/FlowOps.scala
+++ core/src/main/scala/ox/flow/FlowOps.scala
@@ -88,12 +88,21 @@ class FlowOps[+T]:
 
   /** Remove subsequent, repeating elements
     */
-  def debounce: Flow[T] = Flow.usingEmitInline: emit =>
-    var previousElement: Option[T] = None
+  def debounce: Flow[T] =
+    debounceBy(T => T)
+
+  /** Remove subsequent, repeating elements matching 'f'
+    *
+    * @param f
+    *   The function used to compare the previous and current element
+    */
+  def debounceBy[U](f: T => U): Flow[T] = Flow.usingEmitInline: emit =>
+    var previousElement: Option[U] = None
     last.run(
       FlowEmit.fromInline: t =>
-        if !previousElement.contains(t) then emit(t)
-        previousElement = Some(t)
+        val currentElement = f(t)
+        if !previousElement.contains(currentElement) then emit(t)
+        previousElement = Some(currentElement)
     )
 
   /** Applies the given mapping function `f` to each element emitted by this flow, for which the function is defined, and emits the result.
